68c93a9b757ef0816e2b261ce6644b3211629adc
Gomezjose47 <gomezjose47@mail.fresnostate.edu>
2020-12-17 12:28:08 -0800

    finished structured english

--- FinalProject.py
+++ FinalProject.py
@@ -1,4 +1,19 @@
 """
+This program will tell a user how high a ball will go in the air, and the amount
+of time it will take to hit the ground. The program will first obtain the initial heigh from where
+the ball is thrown and the initial velocity that the ball is thrown from. This information will come from
+the user as an input function. 
+
+First the program will request the initial height and velocity from the user. With this information,
+we will use the formula (h + vt - 16t^2) to calculate the max height of the ball. After calculating the
+height, the program will loop and calculate the height every .1 seconds until the height is no longer positive. 
+
+After the program is done doing it's calculations, it should output the maximum height of the ball
+and when the ball will hit the ground as well as the two input functions for the user to change the numbers
+however they would like. 
+"""
+
+"""
 
 
 """
